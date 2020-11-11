d7fd036362c13bf7a3f62ba8aff5be9fc8adabf4
Alan O'Cull <masteralan2001@gmail.com>
2020-11-11 16:15:04 -0500

    Point class

---
+++ src/Objects/Art/point.cpp
@@ -0,0 +1,21 @@
+#include "src/Objects/Art/point.h"
+
+Point::Point(glm::vec2 position) {
+    pos = position;
+    leftHandle = glm::vec2(0, 0);
+    rightHandle = glm::vec2(0, 0);
+}
+
+
+// Blends two points using the given alpha
+Point* POINT_Blend(Point* a, Point* b, float alpha) {
+    Point* ret = new Point(lerp(a->pos, b->pos, alpha));
+
+    // TODO: Consider that right handle of point A corresponds to left handle of point B, and so on
+    // Currently we blend left + left, and right + right as this point is supposed to just be an in-between
+    ret->leftHandle = lerp(a->leftHandle, b->leftHandle, alpha);
+    ret->rightHandle = lerp(a->rightHandle, b->rightHandle, alpha);
+
+    ret->thickness = lerp(a->thickness, b->thickness, alpha);
+    ret->opacity = lerp(a->thickness, b->thickness, alpha);
+}
---
+++ src/Objects/Art/point.h
@@ -0,0 +1,18 @@
+#include "src/config.h"
+#include "src/Util/math.h"
+
+#include <glm/vec2.hpp>
+
+class Point {
+    public:
+        Point(glm::vec2 position);
+
+        glm::vec2 pos;
+        glm::vec2 leftHandle;
+        glm::vec2 rightHandle;
+
+        float thickness = 1.0f;
+        float opacity = 1.0f;
+};
+
+Point* POINT_Blend(Point* a, Point* b, float alpha);
